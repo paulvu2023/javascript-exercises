3eb0f722584fff18005379796a480db46e76b326
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-03 18:01:32 -0700

    Submit exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...manyArgs) {
+    const args = Array.from(arguments);
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 1; j < args.length; j++) {
+            if (array[i] === args[j]) {
+                array.splice(i, 1);
+                i=0;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
