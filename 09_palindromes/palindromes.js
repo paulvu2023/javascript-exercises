24f26a4355fccc4b2324b7b285db8644c64ec9db
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-10 21:57:09 -0700

    Submit palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase().replace(/\W/g, '');
+    reverse = word;
+    reverse = reverse.split('').reverse().join('');
+    if (word === reverse) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
