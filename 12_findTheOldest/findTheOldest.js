f498525cccb8dd018eed1588512ec3dde8e240cf
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-11 09:57:07 -0700

    Submit solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldestIndex = 0;
+    let oldestAge = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if ('yearOfDeath' in arr[i]) {
+            if ((arr[i].yearOfDeath - arr[i].yearOfBirth) > oldestAge) {
+                oldestIndex = i;
+                oldestAge = arr[i].yearOfDeath - arr[i].yearOfBirth;
+            }
+        } else {
+            let year = new Date().getFullYear();
+            if (year - arr[i].yearOfBirth > oldestAge) {
+                oldestIndex = i;
+                oldestAge = year - arr[i].yearOfBirth;
+            }
+        }
+    }
+    return arr[oldestIndex];
 };
 
 // Do not edit below this line
