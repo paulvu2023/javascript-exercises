d4ab04a862a110dc13de8a9386e266dc68d6a6bc
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-10 21:20:48 -0700

    Submit calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	finalSum = arr.reduce((sum, current) => sum + current, 0);
+  return finalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  product = 1;
+  numbers.forEach(number => {
+    product *= number;
+  })
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let answer = base;
+  for (let i = 1; i < exponent; i++) {
+    answer *= base;
+  }
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+  let answer = num;
+  for (let i = num - 1; i > 0; i--) {
+    answer = answer * i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
