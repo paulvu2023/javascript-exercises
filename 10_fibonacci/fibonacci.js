5f95542899ad7251480e3a4f8fe287ae810634af
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-10 22:12:37 -0700

    Submit fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    arr = [0, 1]
+    for (let i = 2; i <= num; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[num];
 };
 
 // Do not edit below this line
